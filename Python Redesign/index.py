98572f5f712f2fa3a33b5ac944a784baa9151f6d
Francois <francoiskrugermail@gmail.com>
2022-04-16 16:30:35 +0200

    More updates

--- Python Redesign/index.py
+++ Python Redesign/index.py
@@ -16,77 +16,77 @@ TIME_SERIES_DAILY_FOLDER = os.path.join(root_folder, 'data', 'TIME_SERIES_DAILY'
 DATA_FROM = 'internet'
 # DATA_FROM = 'jsons'
 
-SHOW_GRAPHS = True
+SHOW_GRAPHS = False
 MINIMAL_OUTPUT = True
 
 STOCKS = [
-    # { 'name': 'Adobe', 'symbol': 'ADBE' },
-    # { 'name': 'Airbnb', 'symbol': 'ABNB' },
-    # { 'name': 'AMD', 'symbol': 'AMD' },
-    # { 'name': 'Apple', 'symbol': 'AAPL' },
-    # { 'name': 'Asana', 'symbol': 'ASAN' },
-    # { 'name': 'Atlassian', 'symbol': 'TEAM' },
-    # { 'name': 'Autodesk', 'symbol': 'ADSK' },
-    # { 'name': 'Beyond Meat', 'symbol': 'BYND' },
-    # { 'name': 'BP', 'symbol': 'BP' },
-    # { 'name': 'Bumble', 'symbol': 'BMBL' },
-    # { 'name': 'Callaway', 'symbol': 'ELY' },
-    # { 'name': 'CAT', 'symbol': 'CAT' },
-    # { 'name': 'Chipolte', 'symbol': 'CMG' },
-    # { 'name': 'Coca Cola', 'symbol': 'KO' },
-    # { 'name': 'Dropbox', 'symbol': 'DBX' },
-    # { 'name': 'EA', 'symbol': 'EA' },
-    # { 'name': 'Ebay', 'symbol': 'EBAY' },
-    # { 'name': 'Etsy', 'symbol': 'ETSY' },
-    # { 'name': 'Facebook/Meta', 'symbol': 'FB' },
-    # { 'name': 'FedEx', 'symbol': 'FDX' },
-    # { 'name': 'Ferrari', 'symbol': 'RACE' },
-    # { 'name': 'Fiverr', 'symbol': 'FVRR' },
-    # { 'name': 'Ford', 'symbol': 'F' },
-    # { 'name': 'Garmin', 'symbol': 'GRMN' },
-    # { 'name': 'GM', 'symbol': 'GM' },
-    # { 'name': 'GoPro', 'symbol': 'GPRO' },
-    # { 'name': 'Hasbro', 'symbol': 'HAS' },
-    # { 'name': 'Honda', 'symbol': 'HMC' },
-    # { 'name': 'IBM', 'symbol': 'IBM' },
-    # { 'name': 'Intel', 'symbol': 'INTC' },
-    # { 'name': 'John Deere', 'symbol': 'DE' },
-    # { 'name': 'Johnson & Johnson', 'symbol': 'JNJ' },
-    # { 'name': 'Kelloggs', 'symbol': 'K' },
+    { 'name': 'Adobe', 'symbol': 'ADBE' },
+    { 'name': 'Airbnb', 'symbol': 'ABNB' },
+    { 'name': 'AMD', 'symbol': 'AMD' },
+    { 'name': 'Apple', 'symbol': 'AAPL' },
+    { 'name': 'Asana', 'symbol': 'ASAN' },
+    { 'name': 'Atlassian', 'symbol': 'TEAM' },
+    { 'name': 'Autodesk', 'symbol': 'ADSK' },
+    { 'name': 'Beyond Meat', 'symbol': 'BYND' },
+    { 'name': 'BP', 'symbol': 'BP' },
+    { 'name': 'Bumble', 'symbol': 'BMBL' },
+    { 'name': 'Callaway', 'symbol': 'ELY' },
+    { 'name': 'CAT', 'symbol': 'CAT' },
+    { 'name': 'Chipolte', 'symbol': 'CMG' },
+    { 'name': 'Coca Cola', 'symbol': 'KO' },
+    { 'name': 'Dropbox', 'symbol': 'DBX' },
+    { 'name': 'EA', 'symbol': 'EA' },
+    { 'name': 'Ebay', 'symbol': 'EBAY' },
+    { 'name': 'Etsy', 'symbol': 'ETSY' },
+    { 'name': 'Facebook/Meta', 'symbol': 'FB' },
+    { 'name': 'FedEx', 'symbol': 'FDX' },
+    { 'name': 'Ferrari', 'symbol': 'RACE' },
+    { 'name': 'Fiverr', 'symbol': 'FVRR' },
+    { 'name': 'Ford', 'symbol': 'F' },
+    { 'name': 'Garmin', 'symbol': 'GRMN' },
+    { 'name': 'GM', 'symbol': 'GM' },
+    { 'name': 'GoPro', 'symbol': 'GPRO' },
+    { 'name': 'Hasbro', 'symbol': 'HAS' },
+    { 'name': 'Honda', 'symbol': 'HMC' },
+    { 'name': 'IBM', 'symbol': 'IBM' },
+    { 'name': 'Intel', 'symbol': 'INTC' },
+    { 'name': 'John Deere', 'symbol': 'DE' },
+    { 'name': 'Johnson & Johnson', 'symbol': 'JNJ' },
+    { 'name': 'Kelloggs', 'symbol': 'K' },
     { 'name': 'Kodak', 'symbol': 'KODK' },
-    # { 'name': 'Logitec', 'symbol': 'LOGI' },
-    # { 'name': 'Manchester United', 'symbol': 'MANU' },
-    # { 'name': 'MasterCard', 'symbol': 'MA' },
-    # { 'name': 'McDonalds', 'symbol': 'MCD' },
-    # { 'name': 'Microsoft', 'symbol': 'MSFT' },
-    # { 'name': 'MongoDB', 'symbol': 'MDB' },
-    # { 'name': 'Monster', 'symbol': 'MNST' },
-    # { 'name': 'Netflix', 'symbol': 'NFLX' },
-    # { 'name': 'Nike', 'symbol': 'NKE' },
-    # { 'name': 'Nokia', 'symbol': 'NOK' },
-    # { 'name': 'Norton', 'symbol': 'NLOK' },
-    # { 'name': 'Nvidia', 'symbol': 'NVDA' },
-    # { 'name': 'PayPal', 'symbol': 'PYPL' },
-    # { 'name': 'Pfizer', 'symbol': 'PFE' },
-    # { 'name': 'Pinterest', 'symbol': 'PINS' },
-    # { 'name': 'Sony', 'symbol': 'SONY' },
-    # { 'name': 'Spotify', 'symbol': 'SPOT' },
-    # { 'name': 'Squarespace', 'symbol': 'SQSP' },
-    # { 'name': 'Tesla', 'symbol': 'TSLA' },
-    # { 'name': 'Toyota', 'symbol': 'TM' },
-    # { 'name': 'Trip Advisor', 'symbol': 'TRIP' },
-    # { 'name': 'Uber', 'symbol': 'UBER' },
-    # { 'name': 'Unity', 'symbol': 'U' },
-    # { 'name': 'UPS', 'symbol': 'UPS' },
-    # { 'name': 'Visa', 'symbol': 'V' },
-    # { 'name': 'Vodafone', 'symbol': 'VOD' },
-    # { 'name': 'Walmart', 'symbol': 'WMT' },
-    # { 'name': 'Walt Disney', 'symbol': 'DIS' },
-    # { 'name': 'Wish', 'symbol': 'WISH' },
-    # { 'name': 'Wix.com', 'symbol': 'WIX' },
-    # { 'name': 'WWE', 'symbol': 'WWE' },
-    # { 'name': 'Zoom', 'symbol': 'ZM' },
-    # { 'name': 'Zynga', 'symbol': 'ZNGA' },
+    { 'name': 'Logitec', 'symbol': 'LOGI' },
+    { 'name': 'Manchester United', 'symbol': 'MANU' },
+    { 'name': 'MasterCard', 'symbol': 'MA' },
+    { 'name': 'McDonalds', 'symbol': 'MCD' },
+    { 'name': 'Microsoft', 'symbol': 'MSFT' },
+    { 'name': 'MongoDB', 'symbol': 'MDB' },
+    { 'name': 'Monster', 'symbol': 'MNST' },
+    { 'name': 'Netflix', 'symbol': 'NFLX' },
+    { 'name': 'Nike', 'symbol': 'NKE' },
+    { 'name': 'Nokia', 'symbol': 'NOK' },
+    { 'name': 'Norton', 'symbol': 'NLOK' },
+    { 'name': 'Nvidia', 'symbol': 'NVDA' },
+    { 'name': 'PayPal', 'symbol': 'PYPL' },
+    { 'name': 'Pfizer', 'symbol': 'PFE' },
+    { 'name': 'Pinterest', 'symbol': 'PINS' },
+    { 'name': 'Sony', 'symbol': 'SONY' },
+    { 'name': 'Spotify', 'symbol': 'SPOT' },
+    { 'name': 'Squarespace', 'symbol': 'SQSP' },
+    { 'name': 'Tesla', 'symbol': 'TSLA' },
+    { 'name': 'Toyota', 'symbol': 'TM' },
+    { 'name': 'Trip Advisor', 'symbol': 'TRIP' },
+    { 'name': 'Uber', 'symbol': 'UBER' },
+    { 'name': 'Unity', 'symbol': 'U' },
+    { 'name': 'UPS', 'symbol': 'UPS' },
+    { 'name': 'Visa', 'symbol': 'V' },
+    { 'name': 'Vodafone', 'symbol': 'VOD' },
+    { 'name': 'Walmart', 'symbol': 'WMT' },
+    { 'name': 'Walt Disney', 'symbol': 'DIS' },
+    { 'name': 'Wish', 'symbol': 'WISH' },
+    { 'name': 'Wix.com', 'symbol': 'WIX' },
+    { 'name': 'WWE', 'symbol': 'WWE' },
+    { 'name': 'Zoom', 'symbol': 'ZM' },
+    { 'name': 'Zynga', 'symbol': 'ZNGA' },
 ]
 
 ##############################################################################################################################
@@ -147,22 +147,24 @@ def run():
         current = None
         for date in data:
             if date > start_date and date < end_date:
-                graph_data[date] = int(data[date].split('.')[0])
-                cumilative_total += graph_data[date]
+                value = float(data[date])
+                graph_data[date] = value
+                cumilative_total += value
                 if not current:
-                    current = graph_data[date]
-                if float(data[date]) > max_value:
-                    max_value = float(data[date])
-                if float(data[date]) < min_value:
-                    min_value = float(data[date])
+                    current = value
+                if value > max_value:
+                    max_value = value
+                if value < min_value:
+                    min_value = value
 
         mid = (max_value + min_value) / 2
         average = cumilative_total / len(graph_data)
         min_to_max_percentage = (max_value - min_value) / min_value * 100
-        current_to_average_percentage = int((current - min_value) / min_value * 100)
-        if current > average:
+        current_to_average_percentage = (current - average) / average * 100
+        if current < average:
             current_to_average_percentage = current_to_average_percentage * -1
         if current_to_average_percentage > best_current_to_average_percentage['value']:
+            best_current_to_average_percentage['name'] = stock['name']
             best_current_to_average_percentage['symbol'] = stock['symbol']
             best_current_to_average_percentage['value'] = current_to_average_percentage
 
@@ -201,12 +203,12 @@ def run():
             print(f'End\t\t\t\t\t{end_date}')
             print()
             print(f'Current\t\t\t\t{current}')
-            print(f'Min\t\t\t\t\t{int(min_value)}')
-            print(f'Max\t\t\t\t\t{int(max_value)}')
-            print(f'Average\t\t\t\t{int(average)}')
+            print(f'Min\t\t\t\t\t{round(min_value, 2)}')
+            print(f'Max\t\t\t\t\t{round(max_value, 2)}')
+            print(f'Average\t\t\t\t{round(average, 2)}')
             print()
-            print(f'Min -> Max\t\t\t{int(min_to_max_percentage)}%')
-        print(f'Current -> Average\t{current_to_average_percentage}% (you want this to be above 20% or higher)')
+            print(f'Min -> Max\t\t\t{round(min_to_max_percentage, 2)}%')
+        print(f'Current -> Average\t{round(current_to_average_percentage, 2)}% (you want this to be above 20% or higher)')
 
         # Labels x, y and title
         pyplot.xlabel('Date')
@@ -216,7 +218,8 @@ def run():
         pyplot.legend() # Show a legend on the plot
         if SHOW_GRAPHS:
             pyplot.show() # Show the plot
-    
-    print(f"BEST CURRENT -> AVERGAGE: {best_current_to_average_percentage['symbol']}:{best_current_to_average_percentage['value']}" )
+
+    print('='*70)
+    print(f"BEST CURRENT -> AVERGAGE:\n{best_current_to_average_percentage['name']}: {round(best_current_to_average_percentage['value'], 2)}%" )
 
 run()
